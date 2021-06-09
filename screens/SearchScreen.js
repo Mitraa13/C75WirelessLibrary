26a4d99542885c494fa0ed7f02f347d738795c98
Mitraa13 <mitraasrinivasan@gmail.com>
2021-06-09 18:18:07 +0530

    C74change

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -4,103 +4,155 @@ import firebase from 'firebase';
 import db from '../config';
 
 export default class SearchScreen extends React.Component{
+
     constructor(props){
-        super(props);
-        this.state={
-            search:'',
-            allTransaction:[],
-            lastVisibleTransaction:null
-        }
+      super(props)
+  
+      this.state = {
+          allTransaction:[],
+          lastVisibleTransaction:null,
+          search:" "
+  
+      }
+  }
+  
+  
+  searchTransaction = async(searchText)=>{
+     var searchText = searchText.toUpperCase()
+    var first_alphabet = searchText.split("")[0]
+  
+    if(first_alphabet == "B"){
+        const transactions = await db.collection("transaction")
+        .where("bookId", '==', searchText)
+        .limit(5)
+        .get()
+        transactions.docs.map(doc=>{
+         this.setState({
+             allTransaction:[...this.state.allTransaction,doc.data()],
+             lastVisibleTransaction:doc
+         })
+        })
+  
     }
-
-    searchTransaction = async (searchText) => {
-        var searchText = searchText.toUpperCase()
-        var first_Alphabet = searchText.split("")[0]
-
-        if(first_Alphabet == "B"){
-            const transactions = await db.collection('transaction').where('bookId','==',searchText).limit(5).get()
-            transactions.docs.map((doc) => {
-                this.setState({
-                    allTransaction:[...this.state,allTransaction,doc.data()],
-                    lastVisibleTransaction:doc
-                })
-            })
-        }
-
-        else if(first_Alphabet == "S"){
-            const transactions = await db.collection('transaction').where('studentId','==',searchText).limit(5).get()
-            transactions.docs.map((doc) => {
-                this.setState({
-                    allTransaction:[...this.state,allTransaction,doc.data()],
-                    lastVisibleTransaction:doc
-                })
-            })
-
-        }
-
-        console.log(this.state.lastVisibleTransaction.id)
+    else if(first_alphabet == "S"){
+      const transactions = await db.collection("transaction")
+      .where("studentId", '==', searchText)
+      .limit(5)
+      .get()
+      transactions.docs.map(doc=>{
+       this.setState({
+           allTransaction:[...this.state.allTransaction,doc.data()],
+           lastVisibleTransaction:doc
+       })
+      })
     }
+    console.log(this.state.lastVisibleTransaction.id)
+  }
+  
+  fetchMore= async()=>{
+      var searchText = this.state.search.toUpperCase()
+      var first_alphabet = searchText.split("")[0]
+  
+      if(first_alphabet == "B"){
+          const transactions = await db.collection("transaction")
+          .where("bookId", '==', searchText)
+          .startAfter(this.state.lastVisibleTransaction)
+          .limit(5)
+          .get()
+          transactions.docs.map(doc=>{
+           this.setState({
+               allTransaction:[...this.state.allTransaction,doc.data()],
+               lastVisibleTransaction:doc
+           })
+          })
+  
+      }
+      else if(first_alphabet == "S"){
+        const transactions = await db.collection("transaction")
+        .where("studentId", '==', searchText)
+        .startAfter(this.state.lastVisibleTransaction)
+        .limit(5)
+        .get()
+        transactions.docs.map(doc=>{
+         this.setState({
+             allTransaction:[...this.state.allTransaction,doc.data()],
+             lastVisibleTransaction:doc
+         })
+        })
+      }
+      console.log(this.state.lastVisibleTransaction.id)
+  }
 
-    render(){
-        return(
-            <View style={styles.container}>
 
-                <TextInput 
-                    style={styles.serchInput}
-                    placeholder="Search Book ID or Student ID"
-                    onChangeText={(text)=>this.setState({search:text})}     
-                />
-                <TouchableOpacity
-                style={styles.button}
-                onPress={()=>{this.searchTransaction()}}>
-                    <Text style={styles.buttonText}>Search</Text>
-                </TouchableOpacity>
-
-                <FlatList 
-                data={this.state.allTransaction}
-                keyExtractor={(items,index)=>{
-                    index.toString()
-                }}
-                renderItem={({item})=>(
-                    <View>
-                        <Text>{"Book ID :" + item.bookId}</Text>
-                        <Text>{"Student ID :" + item.studentId}</Text>
-                        <Text>{"Transaction ID :" + item.transactionType}</Text>
-                        <Text>{"Date :" + item.date.toDate()}</Text>
-                    </View>
-                )}
-                />
-                
-
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor:"aliceblue",
-        justifyContent:"center",
-        alignItems:"center",
-        paddingBottom:100,
-    },
-    serchInput:{
-        alignSelf:"center",
-        alignItems:"center",
-        alignContent:"center",
-        justifyContent:"center",
-        flexDirection:"row",
+  render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.searchBar}>
+        <TextInput 
+          style ={styles.bar}
+          placeholder = "Enter Book Id or Student Id"
+          onChangeText={(text)=>{this.setState({search:text})}}/>
+          <TouchableOpacity
+            style = {styles.searchButton}
+            onPress={()=>{this.searchTransaction(this.state.search)}}
+          >
+            <Text>Search</Text>
+          </TouchableOpacity>
+          </View>
+          <FlatList
+          data ={this.state.allTransaction}
+          keyExtractor = {(items,index)=>{index.toString}}
+          renderItem={({item})=>(
+              <View style = {{borderBottomWidth:3}}>
+                  <Text>{"Book Id :" + item.bookId}</Text>
+                  <Text>{"Student Id:"+ item.studentId}</Text>
+                  <Text>{"Transaction Type :"+ item.transactiontype}</Text>
+                  <Text>{"Date :"+ item.date.toDate()}</Text>
+              </View>
+          )
+  
+          }
+          onEndReached = {()=>{
+              this.fetchMore()
+  
+          }}
+          onEndReachedThreshold={0.7}
+          />
+          </View>
+    );
+  }
+  }
+  
+  
+  
+  const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        marginTop: 20,
+      },
+      searchBar:{
+        flexDirection:'row',
         height:50,
-        borderRadius:10,
-        width:250,
+        width:'auto',
         borderWidth:1.5,
-        margin:20,
-    },
-    buttonText:{
-        fontSize:20,
-        textAlign:"center",
-    },
-    button:{
-        border:"2px solid black",
-    }
-})
+        alignItems:'center',
+        backgroundColor:'white',
+        alignSelf:"center",
+        padding:20
+    
+      },
+      bar:{
+        borderWidth:2,
+        height:40,
+        width:300,
+        paddingLeft:10,
+      },
+      searchButton:{
+        borderWidth:1,
+        height:30,
+        width:50,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'green'
+      }
+    })
